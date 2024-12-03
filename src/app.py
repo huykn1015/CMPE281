cc1928a4c3a804e928737debb53932f25047a551
Huy Nguyen <105837391+huykn1015@users.noreply.github.com>
2024-12-03 15:18:23 -0800

    Update app.py

--- src/app.py
+++ src/app.py
@@ -21,7 +21,7 @@ alert_manager = AlertManager(key_id, key)
 def get_path(schedule_id):
     path = path_manager.get_path(schedule_id)
     if path is not None:
-        return path
+        return jsonify(path)
     stops = schedule_manager.get_schedule(schedule_id)
     if stops is None:
         raise BadRequest(f"Schedule {{{schedule_id}}} does not exist")
